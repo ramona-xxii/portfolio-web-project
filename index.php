a61a6e133237c06ef4de61ee0cbe3493c721498c
Ramona <tester@email.com>
2025-06-30 11:28:07 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ramona's Web Portfolio</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <main>
+            <!-- outer layout -->
+             <div class="outer-layout">
+
+                <!-- side panel icons -->
+                <div class="icon-panel">
+                    <button type="button" title="explorer">
+                        <i class="bi bi-files"></i>
+                    </button>
+                    <button type="button" title="search">
+                        <i class="bi bi-search"></i>
+                    </button>
+                    <button type="button" title="github">
+                        <i class="bi bi-github"></i>
+                    </button>
+                    <button type="button" title="profile">
+                        <i class="bi bi-person-circle"></i>
+                    </button>
+                    <button type="button" title="settings">
+                        <i class="bi bi-gear"></i>
+                    </button>
+                </div>
+
+                 <!-- sidebar navigation -->
+                <nav class="nav flex-column">
+                    <a class="nav-link active" aria-current="page" href="#">homepage.html</a>
+                    <a class="nav-link" href="#">about-me.css</a>
+                    <a class="nav-link" href="#">projects.js</a>
+                    <a class="nav-link" href="#">contact-me.php</a>
+                </nav>
+
+             </div>
+            
+
+           
+
+        </main>
+    </body>
+    
+</html>
