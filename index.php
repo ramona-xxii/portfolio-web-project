239c3a5b64ffa3274ec523865af42539901f5838
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-07-07 14:50:39 -0400

    redid base html structure

--- index.php
+++ index.php
@@ -1,50 +1,59 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Ramona's Web Portfolio</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+        <title>Ramona | Portfolio</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="main.css">
     </head>
     <body>
         <main>
-            <!-- outer layout -->
-             <div class="outer-layout">
 
-                <!-- side panel icons -->
-                <div class="icon-panel">
-                    <button type="button" title="explorer">
-                        <i class="bi bi-files"></i>
-                    </button>
-                    <button type="button" title="search">
-                        <i class="bi bi-search"></i>
-                    </button>
-                    <button type="button" title="github">
-                        <i class="bi bi-github"></i>
-                    </button>
-                    <button type="button" title="profile">
-                        <i class="bi bi-person-circle"></i>
-                    </button>
-                    <button type="button" title="settings">
-                        <i class="bi bi-gear"></i>
-                    </button>
-                </div>
+            <!-- navigation --> 
+            <nav class="topnav">
+                <!-- brand -->
+                <a class="brand" href="#">RAMONA</a>
+                <!-- nav items -->
+                 <div class="nav-items">
+                    <a href="#about" class="nav-link">ABOUT ME</a>
+                    <a href="#projects" class="nav-link">PROJECTS</a>
+                    <button id="nav-contact-btn" type="button">CONTACT</button>
+                 </div>
+            </nav>
+            
+            <!-- content -->
+            <div class="content">
+                <!-- landing section -->
+                <section id="landing-sctn" class="section">
+                    <p>Hi there, I'm</p>
+                    <h1>Ramona!</h1>
+                    <h2>CS student. Freelance developer. Got an idea?</h2>
+                    <div id="landing-btn-contaier">
+                        <button id ="landing-cta-btn" type="button">Let's Build It!</button>
+                    </div>
+                </section>
 
-                 <!-- sidebar navigation -->
-                <nav class="nav flex-column">
-                    <a class="nav-link active" aria-current="page" href="#">homepage.html</a>
-                    <a class="nav-link" href="#">about-me.css</a>
-                    <a class="nav-link" href="#">projects.js</a>
-                    <a class="nav-link" href="#">contact-me.php</a>
-                </nav>
+                <!-- about section -->
+                <section id="about-sctn" class="section">
+                    <h1>About Me</h1>
 
-             </div>
+                </section>
+
+                <!-- projects section -->
+                <section id="projects-sctn" class="section">
+                    
+                </section>
+
+                <!-- contact section -->
+                <section id="contact-sctn" class="section">
+                    
+                </section>
+            </div>
             
 
-           
-
+            
         </main>
     </body>
     
