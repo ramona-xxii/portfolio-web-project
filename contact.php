711adcc66b7b23fa4f21e869c6e6180d8a0642d5
Ramona <tester@email.com>
2025-07-09 15:53:44 -0400

    added email validation

--- contact.php
+++ contact.php
@@ -7,5 +7,23 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
     $phonenumber = htmlspecialchars(trim($_POST["phonenumber"]));
     $message = htmlspecialchars(trim($_POST["message"]));
 
-    
+    // validate name
+        // not empty
+        // not null
+        // no numbers
+    if($name === "" || $name === NULL){
+        // invalid name
+    }
+
+    // auto add dashes for phone number
+
+    // validate email
+    if(!(filter_var($email, FILTER_VALIDATE_EMAIL))){
+        // invalid email error
+    }
+
+    // validate message
+        // not empty
+        // not null
+
 }
